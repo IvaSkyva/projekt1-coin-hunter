9fae06a2489f0a053ee8a2352302d87f0a3d24e7
Ivana Skýva <ivana.rybakova@gmail.com>
2021-03-27 19:12:44 +0100

    Pohyb panačka šipkami

--- main.js
+++ main.js
@@ -7,3 +7,28 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+
+
+document.onkeydown = detectKey;
+function detectKey(e) {
+    var posLeft = document.getElementById('panacek').offsetLeft;
+    var posTop = document.getElementById('panacek').offsetTop;
+    e = e || window.event;
+    if (e.keyCode == '38') {
+        // up arrow
+        document.getElementById('panacek').style.marginTop  = (posTop-58)+"px";
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+        document.getElementById('panacek').style.marginTop  = (posTop+58)+"px";
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+        document.getElementById('panacek').style.marginLeft  = (posLeft-58)+"px";
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+        document.getElementById('panacek').style.marginLeft  = (posLeft+58)+"px";
+    }
+}
+  
