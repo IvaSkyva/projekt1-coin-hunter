73fe1009ae723d01c0e56c021ea165ede6fe5c29
Ivana Skýva <ivana.rybakova@gmail.com>
2021-03-29 14:49:26 +0200

    panacek hranice obrazovky

--- main.js
+++ main.js
@@ -9,26 +9,42 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 // sem začni psát svůj program
 
 
+
+
+let w = screen.width;
+let h = screen.height;
+
 document.onkeydown = detectKey;
+
 function detectKey(e) {
-    var posLeft = document.getElementById('panacek').offsetLeft;
-    var posTop = document.getElementById('panacek').offsetTop;
+    let posLeft = document.getElementById('panacek').offsetLeft;
+    let posTop = document.getElementById('panacek').offsetTop;
+    
+    
     e = e || window.event;
     if (e.keyCode == '38') {
         // up arrow
+        if (posTop > 0) {
         document.getElementById('panacek').style.marginTop  = (posTop-58)+"px";
+        }
     }
     else if (e.keyCode == '40') {
         // down arrow
+        if(posTop < h - 250) {
         document.getElementById('panacek').style.marginTop  = (posTop+58)+"px";
+        }
     }
     else if (e.keyCode == '37') {
        // left arrow
+        if(posLeft > 0) {
         document.getElementById('panacek').style.marginLeft  = (posLeft-58)+"px";
+        }
     }
     else if (e.keyCode == '39') {
        // right arrow
+        if(posLeft < w - 160) {
         document.getElementById('panacek').style.marginLeft  = (posLeft+58)+"px";
+        }
     }
 }
   
