1f04831251812e27c738d04e381d3e5e774502a3
Ivana Skýva <ivana.rybakova@gmail.com>
2021-04-04 11:40:12 +0200

    detekce kolize

--- main.js
+++ main.js
@@ -12,53 +12,77 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 let w = screen.width;
 let h = screen.height;
 let panacek = document.getElementById('panacek');
+let mince= document.querySelector('#mince');
+let panacekSirka = panacek.width;
+let panacekVyska = panacek.height;
+let minceSirka = mince.width;
+let minceVyska = mince.height;
+
 
 
 document.onkeydown = detectKey;
 
 function detectKey(e) {
     
-    let posLeft = document.getElementById('panacek').offsetLeft;
-    let posTop = document.getElementById('panacek').offsetTop;
+    let posLeft = panacek.offsetLeft;
+    let posTop = panacek.offsetTop;
        
     e = e || window.event;
     if (e.keyCode == '38') {
         // up arrow
         if (posTop > 0) {
-            panacek.style.top  = (posTop-10)+"px";
+            panacek.style.top  = (posTop-60)+"px";
+
         }
     }
     else if (e.keyCode == '40') {
         // down arrow
         if(posTop < h - 250) {
-            panacek.style.top  = (posTop+10)+"px";
+            panacek.style.top  = (posTop+60)+"px";
         }
     }
     else if (e.keyCode == '37') {
        // left arrow
         if(posLeft > 0) {
-            panacek.style.left  = (posLeft-10)+"px";
+            panacek.style.left  = (posLeft-60)+"px";
         }
     }
     else if (e.keyCode == '39') {
        // right arrow
         if(posLeft < w - 160) {
-            panacek.style.left  = (posLeft+10)+"px";
+            panacek.style.left  = (posLeft+60)+"px";
         }
     }
-    console.log(panacek);
+
+    detekujKolizi();
 }
+
+function detekujKolizi() {
+    let panacekX = panacek.offsetLeft;
+    let panacekY = panacek.offsetTop;
+    let minceX = mince.offsetLeft;
+    let minceY = mince.offsetTop;
+
+    if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
+        // panacek a mince se prekryvaji
+        console.log("kolize");
+        
+    }
+
+}
+
+
 
 let getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
 
-let mince= document.querySelector('#mince');
 
-// setInterval(() => {
+
+setInterval(() => {
 
    mince.style.left= getRandom(0, (w - 200))+'px'; //  Horizontally
    mince.style.top = getRandom(0, (h - 200))+'px'; //  Vertically
    console.log(mince); 
- // }, 6000); // every 6 seconds
+  }, 3000); // every 3 seconds
 
  
 
@@ -68,20 +92,4 @@ let mince= document.querySelector('#mince');
  let r2 = mince.getBoundingClientRect();
  console.log(r2.top, r2.right, r2.bottom, r2.left);
 
-
-
- let panacekSirka = panacek.width;
- let panacekVyska = panacek.height;
- let minceSirka = mince.width;
- let minceVyska = mince.height;
- 
- let minceX = mince.style.left;
- let minceY = mince.style.top;
- 
- let panacekX = panacek.style.left;
- let panacekY = panacek.style.top;
-
- console.log(panacekX);
- console.log(panacekY);
- 
 
